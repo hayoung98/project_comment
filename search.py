75084e233d110366962ca0872aa8ab9cb4d45006
hayoung98 <51940490+hayoung98@users.noreply.github.com>
2019-07-16 21:15:36 +0800

    更新搜尋單一景點
    1. 搜尋單一景點
    2. 列出幾則評論

--- search.py
+++ search.py
@@ -5,28 +5,46 @@ from bs4 import BeautifulSoup
 google_url = 'https://www.google.com.tw/search'
 
 # 查詢參數
-place=['虎頭山評論','石門水庫評論','永安漁港評論','竹圍漁港評論','大溪老街評論']
-for j in place:
-  my_params = {'q': j}
-
-
-  r = requests.get(google_url, params = my_params)
-
+inputname = input('請輸入景點：')
+j = inputname + '評論'
+my_params = {'q': j}
+r = requests.get(google_url, params = my_params)
 # 確認是否下載成功
-  if r.status_code == requests.codes.ok:
+if r.status_code == requests.codes.ok:
   # 以 BeautifulSoup 解析 HTML 原始碼
-     soup = BeautifulSoup(r.text, 'html.parser')
-
-
-   #print(soup.prettify())
-
-
-     items = soup.select('a[href^="/url"]')
-     for i in items:
-      if(i.get('href')[0:55]=='/url?q=https://www.tripadvisor.com.tw/Attraction_Review'):
-
+    soup = BeautifulSoup(r.text, 'html.parser')
+  #print(soup.prettify())
+    items = soup.select('a[href^="/url"]')
+    for i in items:
+        if(i.get('href')[0:55]=='/url?q=https://www.tripadvisor.com.tw/Attraction_Review'):
+            attraction_url = i.get('href')[7:]
     # 標題
-         print("標題：" + i.text)
+           #print("標題：" + i.text)
     # 網址
-         print("網址：" + i.get('href')[7:])
+           #print("網址：" + i.get('href')[7:])
 
+res = requests.get(attraction_url)
+html_doc = res.text
+
+# 以 Beautiful Soup 解析 HTML 程式碼
+soup = BeautifulSoup(html_doc, 'html.parser')
+f = open('comment.txt', 'w')
+
+h1_tags = soup.find_all('li', class_="breadcrumb")
+for h1_tag in h1_tags:
+    if h1_tag.string is None:
+        pass
+    else:
+        print("您所搜尋的景點為：",h1_tag.string,"\n")
+
+any_tags = soup.find_all('p', class_="partial_entry")
+i=1
+for tag in any_tags:
+    comment = tag.string
+    if comment is None:
+        pass  # 過濾None
+    else:
+        print(i,".\n",str(comment), "\n")
+        i=i+1
+
+f.close()
