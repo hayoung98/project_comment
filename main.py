c7308f3bf36dc7b665ea61833d313af496876547
hayoung98 <51940490+hayoung98@users.noreply.github.com>
2019-07-16 17:41:58 +0800

    10筆評論
    可以爬10筆評論,tripadvisor

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ html_doc = res.text
 
 # 以 Beautiful Soup 解析 HTML 程式碼
 soup = BeautifulSoup(html_doc, 'html.parser')
+f = open('comment.txt','w')
+
+h1_tags = soup.find_all('li',class_="breadcrumb")
+for h1_tag in h1_tags:
+    if h1_tag.string is None:
+        pass
+    else:
+        print(h1_tag.string)
+        f.write(h1_tag.string)
+
 any_tags = soup.find_all('p',class_="partial_entry")
 for tag in any_tags:
-    if tag.string is None:
+    comment = tag.string
+    if comment is None:
         pass #過濾
     else:
-        print(tag.string,"\n")
+        print(str(comment),"\n")
+        f.write(comment)
+        f.write("\n")
+
+f.close()
+
 
