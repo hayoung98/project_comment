07dd7acca6f808481e8cfbc09b1e667fc3b8ed1a
hayoung98 <51940490+hayoung98@users.noreply.github.com>
2019-07-29 16:07:53 +0800

    更新忽略、解決一些bug
    1.忽略掉沒有評論的景點
    2.有些景點會找到不相關的景點評論
    3.是否要在關鍵字前面加上"桃園"?

--- search.py
+++ search.py
@@ -5,7 +5,6 @@ import json
 url = "https://data.tycg.gov.tw/api/v1/rest/datastore/bd906b29-9006-40ed-8bd7-67597c2577fc?format=json"
 r = requests.get(url)
 data = json.loads(r.text)
-#print((data['result']['records'][0]['Name']))
 
 for index in range(100):
 # Google 搜尋 URL
@@ -13,38 +12,29 @@ for index in range(100):
 
 # 查詢參數
     inputname = data['result']['records'][index]['Name']
-    j = inputname + '評論'
+    j = inputname + ' Attraction_Review'
     my_params = {'q': j}
     r = requests.get(google_url, params = my_params)
-    print("--------------------------------------------------------------------------------------------------------------------------------")
-    print("景點：",inputname)
+
 # 確認是否下載成功
     if r.status_code == requests.codes.ok:
-  # 以 BeautifulSoup 解析 HTML 原始碼
         soup = BeautifulSoup(r.text, 'html.parser')
-  #print(soup.prettify())
         items = soup.select('a[href^="/url"]')
+        attraction_url = ''
         for i in items:
-            if(i.get('href')[0:37]=='/url?q=https://www.tripadvisor.com.tw'):
+            if(i.get('href')[0:55]=='/url?q=https://www.tripadvisor.com.tw/Attraction_Review'):
                 attraction_url = i.get('href')[7:]
-    # 標題
-           #print("標題：" + i.text)
-    # 網址
-                print("網址：" + i.get('href')[7:])
 
-
+    if len(attraction_url)==0:
+        continue
+    print("--------------------------------------------------------------------------------------------------------------------------------")
+    print("景點：", inputname)
+    print("網址：" + attraction_url)
     res = requests.get(attraction_url)
     html_doc = res.text
 
 # 以 Beautiful Soup 解析 HTML 程式碼
     soup = BeautifulSoup(html_doc, 'html.parser')
-
-    # h1_tags = soup.find_all('li', class_="breadcrumb")
-    # for h1_tag in h1_tags:
-    #     if h1_tag.string is None:
-    #         pass
-    #     else:
-    #         print("您所搜尋的景點為：",inputname)
 
     label_tags = soup.find_all('label',class_="row_label label",limit=5)
     span_tags = soup.find_all('span',class_="row_num is-shown-at-tablet")
@@ -56,23 +46,22 @@ for index in range(100):
         spanlist.append(span_tag.string)
     dictionary = dict(zip(labellist, spanlist))
     print("評分: ",end='')
-    print(dictionary,"\n")
-# for x in dictionary:
-#     print(x)
-#     for y in dictionary[x]:
-#         print(y,end='')
 
+    if bool(dictionary) == False:
+        print('目前尚無評分')
+    else:
+        print(dictionary,"\n")
 
     print("****************評論****************")
     any_tags = soup.find_all('p', class_="partial_entry")
     i=1
     for tag in any_tags:
         comment = tag.string
-        if comment is None:
+        if (len(attraction_url)==0) or (comment is None):
             pass  # 過濾None
         else:
             print(i,".\n",str(comment), "\n")
             i=i+1
-    print("****************評論****************")
     any_tags.clear()
+
 print("--------------------------------------------------------------------------------------------------------------------------------")
